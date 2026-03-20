f07c98dab22eee6e505c4ab836e83f8160a792d2
Stezeul <caradeanu.stefan@gmail.com>
2026-03-20 11:44:03 +0200

    reuseable stepmessage component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
     </div>
   );
 }
@@ -60,10 +59,19 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-            {/* {test.name} */}
-          </p>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
@@ -75,6 +83,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
